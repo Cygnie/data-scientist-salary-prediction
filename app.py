4e344fff7fd4b65dea7e88598bae504ccb99f001
Furkan CEYRAN <75915883+Cygnie@users.noreply.github.com>
2022-10-27 20:46:39 +0300

    updates

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request
-import jsonify
-import requests
+import json
+import pickle
+
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
-import sklearn
-import pickle
 import datetime
 
 
@@ -26,46 +25,43 @@ def predict():
                                        'CA', 'NG', 'EE', 'CZ', 'BE', 'ES', 'AT', 'BR', 'DZ', 'IR', 'NL',
                                        'HU', 'PT', 'TR', 'JP', 'HN', 'MT', 'AE', 'LU', 'RO', 'SG', 'CH',
                                        'MY', 'MX', 'IT', 'HR', 'IL', 'DK', 'RU', 'CO', 'IQ', 'CN']
-    if request.method == 'POST':
         
-        Year = int(request.form['Year'])
-        year = int(datetime.datetime.now().year) - Year
-        
-        experience_level=request.form['experience_level']
-        employment_type=request.form['employment_type']
-        job_title=request.form['job_title']
-        
-        employee_residence = request.form['employee_residence']
-        
-        if employee_residence.strip().upper() not in employee_residence_country_list:
-           employee_residence ="Other"
-        
-        company_location = request.form['company_location']
-        
-        if company_location.strip().upper() not in company_location_country_list:
-           company_location ="Other"
-        
-        remote_ratio=request.form['remote_ratio']
-        company_size=request.form['company_size']
-        
-        prediction_table = {"experience_level":experience_level, 
-                            "employment_type":employment_type, 
-                            "job_title":job_title,
-                            "employee_residence":employee_residence, 
-                            "remote_ratio":remote_ratio, 
-                            "company_location":company_location,
-                            "company_size":company_size, 
-                            "year":year}
-        prediction_DF = pd.DataFrame(data=[prediction_table])
+    Year = int(request.form['Year'])
+    year = int(datetime.datetime.now().year) - Year
+    
+    experience_level=request.form['experience_level']
+    employment_type=request.form['employment_type']
+    job_title=request.form['job_title']
+    
+    employee_residence = request.form['employee_residence']
+    
+    if employee_residence.strip().upper() not in employee_residence_country_list:
+        employee_residence ="Other"
+    
+    company_location = request.form['company_location']
+    
+    if company_location.strip().upper() not in company_location_country_list:
+        company_location ="Other"
+    
+    remote_ratio=request.form['remote_ratio']
+    company_size=request.form['company_size']
+    
+    prediction_table = {"experience_level":experience_level, 
+                        "employment_type":employment_type, 
+                        "job_title":job_title,
+                        "employee_residence":employee_residence, 
+                        "remote_ratio":remote_ratio, 
+                        "company_location":company_location,
+                        "company_size":company_size, 
+                        "year":year}
+    prediction_DF = pd.DataFrame(data=[prediction_table])
 
-        output = model.predict(prediction_DF)
+    output = model.predict(prediction_DF)
 
-        if output<0:
-            return render_template('home.html',prediction_text="Sorry you cannot predict your salary")
-        else:
-            return render_template('home.html',prediction_text="your estimated salary is {}".format(output[0]))
+    if output<0:
+        return render_template('home.html',prediction_text="Sorry you cannot predict your salary")
     else:
-        return render_template('home.html')
+        return render_template('home.html',prediction_text="your estimated salary is {}".format(output[0]))
 
 if __name__=="__main__":
     app.run(debug=True)
